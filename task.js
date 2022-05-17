5a725982e42bcc53fdb7afcf8ea5b7d07ee5aae5
roymeir <roy.meir@post.idc.ac.il>
2022-05-17 14:40:51 +0300

    Final commit for v1. List works. You can add and delete tasks and there's slight animation at play. Next, I'll introduce React to make the syntax simpler and move on to improve and add features.

--- task.js
+++ task.js
@@ -1,40 +1,22 @@
-const form = document.getElementById('Form');
-const inupt = document.getElementById('Input');
-const taskList = document.getElementById('Task_list');
+const form = document.getElementById("Form");
+const input = document.getElementById("Input");
+const taskList = document.getElementById("Task_list");
+
+console.log(form, input, taskList);
 
 form.onsubmit = (e) => {
   e.preventDefault();
   addTask();
 };
 
-// function addTask(task) {
-//   let taskValue = input.value;
-//   task ? (taskValue = task.text) : task;
-//
-//   taskValue ? displayTask() : taskValue;
-//
-//   function displayTask() {
-//     const newTask = document.createElement('li');
-//
-//     newTask.innerHTML = taskValue;
-//
-//     newTask.onClick = () => {
-//       newTask.classList.toggle('completed');
-//       newTask.classList.contains('completed') ? ((newTask.innerHTML = `<span> ${taskValue} &#1004; </span>`
-//         `<span onClick = 'deleteTask()' > & #10060 </span>`))
-//       classList.toggle('delete_task') : ((newTask.innerHTML = '${taskValue}', newTask.classList.toggle('delete_task')));
-//     };
-//
-//     taskList.appendChild(newTask);
-//     input.value = "";
-//   }
-// }
-// }
-
 function addTask(task) {
   let taskValue = input.value;
+  //   if (task) {
+  //     taskValue = task.text;
+  //   }
 
   task ? (taskValue = task.text) : task;
+  console.log(taskValue);
   taskValue ? displayTask() : taskValue;
 
   function displayTask() {
@@ -44,7 +26,7 @@ function addTask(task) {
 
     newTask.onclick = () => {
       newTask.classList.toggle("completed");
-
+      //   console.log(newTask);
       newTask.classList.contains("completed")
         ? ((newTask.innerHTML = `<span>${taskValue} &#10004;</span><span onclick='deleteTask()'>&#10060</span>`),
           newTask.classList.toggle("delete_task"))
@@ -56,4 +38,14 @@ function addTask(task) {
 
     input.value = "";
   }
+}
+
+// delete task function
+
+function deleteTask() {
+  console.log(document.querySelectorAll("li"));
+
+  document.querySelectorAll("li").forEach((element) => {
+    element.classList.contains("delete_task") ? element.remove() : element;
+  });
 }
