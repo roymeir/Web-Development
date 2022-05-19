953abd4f1f00a6f8e45fbe6a48616e9a2facce1d
roymeir <roy.meir@post.idc.ac.il>
2022-05-19 20:17:36 +0300

    Fixed "Select All" button. Still has error when deleting multiple tasks.

--- task.js
+++ task.js
@@ -2,6 +2,10 @@ const form = document.getElementById("Form");
 const input = document.getElementById("Input");
 const taskList = document.getElementById("Task_list");
 
+const selectAll_button = document.createElement("li");
+$(selectAll_button).html("Select All");
+taskList.appendChild(selectAll_button);
+
 console.log(form, input, taskList);
 
 form.onsubmit = (e) => {
@@ -11,40 +15,43 @@ form.onsubmit = (e) => {
 
 function addTask(task) {
   let taskValue = input.value;
-  //   if (task) {
-  //     taskValue = task.text;
-  //   }
 
   task ? (taskValue = task.text) : task;
-  console.log(taskValue);
+
   taskValue ? displayTask() : taskValue;
 
   function displayTask() {
     const newTask = document.createElement("li");
-
-    newTask.innerHTML = taskValue;
-
-    newTask.onclick = () => {
-      newTask.classList.toggle("completed");
-      //   console.log(newTask);
-      newTask.classList.contains("completed")
-        ? ((newTask.innerHTML = `<span>${taskValue} &#10004;</span><span onclick='deleteTask()'>&#10060</span>`),
-          newTask.classList.toggle("delete_task"))
-        : ((newTask.innerHTML = `${taskValue}`),
-          newTask.classList.toggle("delete_task"));
-      console.log(newTask);
-    };
+    $(newTask).html(taskValue);
     taskList.appendChild(newTask);
 
+    newTask.onclick = () => {
+      select_task(this);
+    };
+
+    selectAll_button.onclick = () => {
+      $('#Task_list li:not(:first)').each(function() {
+        select_task(this);
+      })
+    };
     input.value = "";
   }
+}
+
+
+function select_task(newTask) {
+  newTask.classList.toggle("completed");
+
+  newTask.classList.contains("completed") ?
+    ((newTask.innerHTML = `<span>${newTask.innerHTML} &#10004;</span><span onclick='deleteTask()'>&#10060</span>`),
+      newTask.classList.toggle("delete_task")) :
+    ((newTask.innerHTML = `${newTask.innerHTML.charAt(6)}`),
+      newTask.classList.toggle("delete_task"));
 }
 
 // delete task function
 
 function deleteTask() {
-  console.log(document.querySelectorAll("li"));
-
   document.querySelectorAll("li").forEach((element) => {
     element.classList.contains("delete_task") ? element.remove() : element;
   });
